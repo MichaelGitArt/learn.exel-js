bddffcc3440b5539cc6b4161e69463a41a3e60ef
Michael Gitart <michaelgitart@gmail.com>
2020-05-17 21:07:20 +0300

    Prepare for resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,19 @@ import { createTable } from '@/components/table/table.template';
 export class Table extends ExelComponent {
 	static className = 'exel-table';
 
+	constructor($root) {
+		super($root, {
+			listeners: ['mousedown'],
+		});
+	}
+
 	toHTML() {
 		return createTable(20);
+	}
+
+	onMousedown(event) {
+		if (event.target.dataset.resize) {
+			console.log('Start resize');
+		}
 	}
 }
